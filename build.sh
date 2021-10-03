ff659aded854a4962430ac6813b4de857d17fe2e
Maxim Polyakov <max.senia.poliak@gmail.com>
2021-10-04 00:29:46 +0300

    Add script option to build cross compiler
    ./build.sh cross
    
    Signed-off-by: Maxim Polyakov <max.senia.poliak@gmail.com>

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ FLAG_BUILD_ALL=0
 FLAG_CLEAR_ALL=0
 FLAG_REBUILD_APP=0
 FLAG_BUILD_RTEMS=0
+FLAG_BUILD_CROSS=0
 
 function make_preparations
 {
@@ -31,6 +32,7 @@ function make_preparations
 
 function build_cross_compiler
 {
+    rm -Rf ${RTEMS_DIR}/rtems-exe
     echo "#########################################################"
     echo "  Build cross-compiler ..."
     echo "#########################################################"
@@ -49,6 +51,8 @@ function build_cross_compiler
 function build_rtems_os
 {
     rm -Rf ${RTEMS_DIR}/tmp ${RTEMS_DIR}/build
+    export PATH=${RTEMS_DIR}/rtems-exe/bin:$PATH
+    export PATH=${RTEMS_DIR}/rtems/rtems-exe/${RTEMS_ARCH}-rtems5/bin:$PATH
     echo "#########################################################"
     echo "  Build RTEMS OS  ..."
     echo "#########################################################"
@@ -95,6 +99,7 @@ function print_help
     echo "Use $0 [OPTIONS...]"
     echo "  -a | all       Build all: cross-compiler, RTEMS OS and ile-cli application"
     echo "       rtems     Build RTEMS OS"
+    echo "       cross     Build cross-compiler"
     echo "  -C | cleanall  Clear all"
     echo "  -r | rebuild   Set rebuils flag"
     echo "                 Delete the application's object files before building it"
@@ -110,6 +115,11 @@ while [ "${1:-}" != "" ]; do
             ;;
         "rtems")
             FLAG_BUILD_RTEMS=1
+            shift
+            break
+            ;;
+        "cross")
+            FLAG_BUILD_CROSS=1
             shift
             break
             ;;
@@ -140,6 +150,11 @@ if [[ ${FLAG_CLEAR_ALL} -eq 1 ]] ; then
     echo "  Clean all ..."
     echo "#########################################################"
     rm -rf ${ROOT_DIR}/ile-cli-test.exe ${ROOT_DIR}/build/ ${RTEMS_DIR}
+    exit 0
+fi;
+
+if [[ ${FLAG_BUILD_CROSS} -eq 1 ]] ; then
+    build_cross_compiler
     exit 0
 fi;
 
